1616e9d064c5a1d3500cc8b67457b42ceabf386d
Sara <fy23soaa@leeds.ac.uk>
2026-03-03 14:36:50 +0000

    Fibonacci sequence solution

---
+++ python/fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci(n: int) -> list:
+    """Returns a list of the first n Fibonacci numbers."""
+    if n <= 0:
+        return []
+    if n == 1:
+        return [0]
+
+    sequence = [0, 1]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+
+
+if __name__ == "__main__":
+    n = int(input("How many Fibonacci numbers? "))
+    print(fibonacci(n))
